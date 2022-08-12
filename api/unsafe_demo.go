af827a8f10eaeca15ce62e38ecfc5c5000383a1a
fdh3721@163.com <fdh3721@163.com>
2022-08-12 21:12:21 +0800

    unsafe

--- api/unsafe_demo.go
+++ api/unsafe_demo.go
@@ -9,4 +9,8 @@ func main() {
 	var i int = 0
 	var size = unsafe.Sizeof(i)
 	fmt.Println("The size of an int is: ", size)
+	alignof := unsafe.Alignof(i)
+	fmt.Println("The alignof of an int is: ", alignof)
+	//fmt.Println("The alignof of an int is: ", unsafe.Pointer)
+	//fmt.Println("The alignof of an int is: ", unsafe.IntegerType)
 }
